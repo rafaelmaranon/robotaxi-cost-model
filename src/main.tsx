0a4aaf4d1e6beb7a76e7ea1941f9cf381a9e4e29
Rafael Maranon <rafaelmaranon@Rowan2.local>
2026-02-16 15:54:22 -0800

    Restore original App with Recharts - test if issue is resolved
    - Switch back to full App.tsx with charts
    - Will test if Recharts works now or if we need alternative chart solution

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App-no-charts.tsx'
+import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
