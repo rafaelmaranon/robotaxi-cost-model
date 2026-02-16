19e3ba5a3e68439ec01980fc79b7d9ba1694d743
Rafael Maranon <rafaelmaranon@Rowan2.local>
2026-02-16 15:49:39 -0800

    Test build with minimal React app to diagnose white page issue

---
+++ src/App-test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: '20px', backgroundColor: 'lightblue' }}>
+      <h1>Test - Robotaxi Unit Economics Simulator</h1>
+      <p>If you can see this, React is working!</p>
+    </div>
+  )
+}
+
+export default App
